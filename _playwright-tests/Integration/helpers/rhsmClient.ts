97407c02cd717013a55b5648ca7f53a5ff2e77c7
Matej Marusak <marusak.matej@gmail.com>
2026-01-05 16:36:06 +0100

    test: Randomize dmi.system.uuid before registering
    This avoids two containers running in parallel affecting each other.

--- _playwright-tests/Integration/helpers/rhsmClient.ts
+++ _playwright-tests/Integration/helpers/rhsmClient.ts
@@ -107,6 +107,13 @@ export class RHSMClient {
    * @returns
    */
   async RegisterRHC(activationKey?: string, orgId?: string, template?: string) {
+    // Before registering randomize dmi.system.uuid
+    await runCommand(this.name, [
+      'sh',
+      '-c',
+      `echo '{"dmi.system.uuid": "${this.name}"}' > /etc/rhsm/facts/custom-dmi.facts`,
+    ]);
+
     if (!process.env.PROD) {
       await this.ConfigureSubManForStage();
       await this.ConfigureRHCForStage();
